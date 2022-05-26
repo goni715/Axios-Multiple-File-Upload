25fe214762f0832f353a7d11d1373aa89a86de8e
Osman Goni <94566411+goni715@users.noreply.github.com>
2022-05-26 16:07:15 +0600

    File Size

--- blog/resources/js/upload.js
+++ blog/resources/js/upload.js
@@ -8,7 +8,7 @@ $('.addBtn').on('click', function(){
 
       let newTableRow=
                   "<tr>"+
-                     "<td><input class='form-control' type='file'></td>"+
+                     "<td><input class='fileInput form-control' type='file'></td>"+
                      "<td class='fileSize'>File Size</td>"+
                      "<td><button class='btn cancelBtn btn-danger btn-sm'>Cancel</button></td>"+
                      "<td><button class='btn upBtn btn-primary btn-sm'>Upload</button></td>"+
@@ -21,4 +21,34 @@ $('.addBtn').on('click', function(){
         $('.fileList').append(newTableRow);
 
 
+        //Remove Row
+        $('.cancelBtn').on('click',function(){
+
+             $(this).parents('tr').remove();
+  
+         });
+
+
+         //Input File onChange
+         $('.fileInput').on('change',function(){
+
+              let MyFile = $(this).prop('files')[0];
+              let MyFileSize = ((MyFile.size)/(1024*1024)).toFixed(2);
+    
+              $(this).closest('tr').find('.fileSize').html(MyFileSize+ "MB"); 
+    
+        });
+
+
+
+
 });
+
+
+
+ //Remove Row
+  
+
+
+
+   
